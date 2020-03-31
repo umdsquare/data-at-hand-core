1725f6ae202c1b930d5d912187b56d79be476384
Young-Ho Kim <yghokim@umd.edu>
2020-03-31 00:21:51 -0400

    initial commit

---
+++ src/logging/types.ts
@@ -0,0 +1,14 @@
+
+export enum InteractionTransitionLogType {
+    Transition = "trans",
+    VerboseEvent = "event"
+}
+
+export enum VerboseEventTypes {
+    Reset = "Reset",
+    LoggingTurnedOn = "LoggingTurnedOn",
+    LoggingTurnedOff = "LoggingTurnedOff",
+    SpeechFail = "SpeechFail",
+
+    VoidSpeechAction = "VoidSpeech"
+}
